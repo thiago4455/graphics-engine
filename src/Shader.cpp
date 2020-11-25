aeb46e70542053a1f4ed366155e7ebb95370a0ba
Thiago <thiagobuenosairesmattos@gmail.com>
2020-11-25 18:50:35 -0300

    Change repository structure

---
+++ src/Shader.cpp
@@ -0,0 +1,5 @@
+//
+// Created by thiagomattos on 11/23/20.
+//
+
+#include "Shader.h"
---
+++ src/Shader.h
@@ -0,0 +1,14 @@
+//
+// Created by thiagomattos on 11/23/20.
+//
+
+#ifndef PHYS_ENGINE_SHADER_H
+#define PHYS_ENGINE_SHADER_H
+
+
+class Shader {
+
+};
+
+
+#endif //PHYS_ENGINE_SHADER_H
